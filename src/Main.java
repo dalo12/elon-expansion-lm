f13e85d0ad0aad36444d469f46a89c379cb1dfcf
David López <dalo_vidpez@hotmail.com.ar>
2023-05-12 17:47:26 -0300

    Ahora en java compila. No anda bien

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import org.javatuples.Triplet;
 import java.util.Scanner;
 import java.util.List;
 import java.util.Map.Entry;
@@ -13,38 +12,38 @@ public class Main{
         int n = 0;
         int W = 0;
         int L = 0;
-        Triplet<Integer,Integer,Integer>[] P;
+        Triple<Integer,Integer,Integer>[] P;
         Scanner s = new Scanner(System.in);
 
         n = s.nextInt();
-        W = s.nexInt();
+        W = s.nextInt();
         L = s.nextInt();
 
-        P = (Triplet<Integer,Integer,Integer>) new Triplet[n];
+        P = (Triple<Integer,Integer,Integer>[]) new Triple[n];
 
         for(int i=0; i<n; i++){
-            newW = s.nextInt();
-            newL = s.nextInt();
-            P[i] = new Triplet<Integer,Integer,Integer>(newW, newL, i);
+            int newW = s.nextInt();
+            int newL = s.nextInt();
+            P[i] = new Triple<Integer,Integer,Integer>(newW, newL, i);
         }
-    
+
         Entry<Integer, List<Integer>> solucion = expandir(P, W, L);
-        
+
         System.out.println(solucion.getKey());
         for(Integer i : solucion.getValue()){
             System.out.print(i);
         }
     }
 
-    protected static Entry<Integer, List<Integer>> expandir(Triplet<Integer,Integer,Integer>[] P, int W, int L){
-        Entry<Triplet<Integer,Integer,Integer>[], Integer> respuesta = ordenar(P);
-        Triplet<Integer,Integer,Integer>[] pOrdenado = respuesta.getKey();
+    protected static Entry<Integer, List<Integer>> expandir(Triple<Integer,Integer,Integer>[] P, int W, int L){
+        Entry<Triple<Integer,Integer,Integer>[], Integer> respuesta = ordenar(P);
+        Triple<Integer,Integer,Integer>[] pOrdenado = respuesta.getKey();
         int maxIndice = respuesta.getValue() - 1;
         int[] R = new int[respuesta.getValue()];
         List<Integer> indicesPlanes = new ArrayList<Integer>();
-        
+
         if(respuesta.getValue() > 0){
-            if( W < pOrdenado[maxIndice].getValue0() and L < pOrdenado[maxIndice].getValue1() ){
+            if( W < pOrdenado[maxIndice].getValue0() && L < pOrdenado[maxIndice].getValue1() ){
                 R[maxIndice] = 1;
                 W = pOrdenado[maxIndice].getValue0();
                 L = pOrdenado[maxIndice].getValue1();
@@ -54,7 +53,7 @@ public class Main{
             }
 
             for(int i=maxIndice-1; i>=0; i--){
-                if(W < pOrdenado[i].getValue0() and L < pOrdenado[i].getValue1()){
+                if(W < pOrdenado[i].getValue0() && L < pOrdenado[i].getValue1()){
                     R[i] = 1 + R[i+1];
                     W = pOrdenado[i].getValue0();
                     L = pOrdenado[i].getValue1();
@@ -68,17 +67,17 @@ public class Main{
         return new SimpleEntry(R[0], indicesPlanes);
     }
 
-    protected static Entry<Triplet<Integer,Integer,Integer>[],Integer> odenar(Triplet<Integer,Integer,Integer>[] P){
-        Arrays.sort(P, new Comparator<Triplet<Integer,Integer,Integer>>(){
+    protected static Entry<Triple<Integer,Integer,Integer>[],Integer> ordenar(Triple<Integer,Integer,Integer>[] P){
+        Arrays.sort(P, new Comparator<Triple<Integer,Integer,Integer>>(){
 
             @Override
-            public int compare(Triplet<Integer,Integer,Integer> arg0, Triplet<Integer,Integer,Integer> arg1){
+            public int compare(Triple<Integer,Integer,Integer> arg0, Triple<Integer,Integer,Integer> arg1){
                 return arg0.getValue0() - arg1.getValue0();
             }
         });
 
-        Triplet<Integer,Integer,Integer>> pOrdenado = (Triplet<Integer,Integer,Integer>) new Triplet[P.length];
-        j = 0;
+        Triple<Integer,Integer,Integer>[] pOrdenado = (Triple<Integer,Integer,Integer>[]) new Triple[P.length];
+        int j = 0;
 
         for (int i=0; i<P.length-1; i++){
             if(P[i].getValue1() > P[i+1].getValue1()){
@@ -87,7 +86,31 @@ public class Main{
         }
 
         pOrdenado[j++] = P[P.length-1];
-        
-        return new SimpleEntry<Triplet<Integer,Integer,Integer>[], Integer>(pOrdenado, j);
+
+        return new SimpleEntry<Triple<Integer,Integer,Integer>[], Integer>(pOrdenado, j);
+    }
+}
+
+class Triple<L,M,R>{
+    protected L left;
+    protected M middle;
+    protected R right;
+
+    public Triple(L left, M middle, R right){
+        this.left = left;
+        this.middle = middle;
+        this.right = right;
+    }
+
+    public L getValue0(){
+        return left;
+    }
+
+    public M getValue1(){
+        return middle;
+    }
+
+    public R getValue2(){
+        return right;
     }
 }
