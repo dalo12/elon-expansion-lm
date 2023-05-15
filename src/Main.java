42173f68d85ce1fea8d7e0026ab0e182d1c8a338
David López <dalo_vidpez@hotmail.com.ar>
2023-05-15 20:21:14 -0300

    Implementado con ordenamiento por w y por l

--- src/Main.java
+++ src/Main.java
@@ -36,38 +36,35 @@ public class Main{
     }
 
     protected static Entry<Integer, List<Integer>> expandir(Triple<Integer,Integer,Integer>[] P, int W, int L){
-        Entry<Triple<Integer,Integer,Integer>[], Integer> respuesta = ordenar(P);
-        Triple<Integer,Integer,Integer>[] pOrdenado = respuesta.getKey();
-        int maxIndice = respuesta.getValue() - 1;
-        int[] R = new int[respuesta.getValue()];
+        Triple<Integer,Integer,Integer>[] pOrdenado = ordenar(P);
+        int maxIndice = pOrdenado.length - 1;
+        int[] R = new int[pOrdenado.length];
         List<Integer> indicesPlanes = new ArrayList<Integer>();
 
-        if(respuesta.getValue() > 0){
-            if( W < pOrdenado[maxIndice].getValue0() && L < pOrdenado[maxIndice].getValue1() ){
-                R[maxIndice] = 1;
-                W = pOrdenado[maxIndice].getValue0();
-                L = pOrdenado[maxIndice].getValue1();
-                indicesPlanes.add(pOrdenado[maxIndice].getValue2());
-            }else{
-                R[maxIndice] = 0;
-            }
+        if( W < pOrdenado[maxIndice].getValue0() && L < pOrdenado[maxIndice].getValue1() ){
+            R[maxIndice] = 1;
+            W = pOrdenado[maxIndice].getValue0();
+            L = pOrdenado[maxIndice].getValue1();
+            indicesPlanes.add(pOrdenado[maxIndice].getValue2());
+        }else{
+            R[maxIndice] = 0;
+        }
 
-            for(int i=maxIndice-1; i>=0; i--){
-                if(W < pOrdenado[i].getValue0() && L < pOrdenado[i].getValue1()){
-                    R[i] = 1 + R[i+1];
-                    W = pOrdenado[i].getValue0();
-                    L = pOrdenado[i].getValue1();
-                    indicesPlanes.add(pOrdenado[i].getValue2());
-                }else{
-                    R[i] = R[i+1];
-                }
+        for(int i=maxIndice-1; i>=0; i--){
+            if(W < pOrdenado[i].getValue0() && L < pOrdenado[i].getValue1()){
+                R[i] = 1 + R[i+1];
+                W = pOrdenado[i].getValue0();
+                L = pOrdenado[i].getValue1();
+                indicesPlanes.add(pOrdenado[i].getValue2());
+            }else{
+                R[i] = R[i+1];
             }
         }
 
         return new SimpleEntry(R[0], indicesPlanes);
     }
 
-    protected static Entry<Triple<Integer,Integer,Integer>[],Integer> ordenar(Triple<Integer,Integer,Integer>[] P){
+    protected static Triple<Integer,Integer,Integer>[] ordenar(Triple<Integer,Integer,Integer>[] P){
         Arrays.sort(P, new Comparator<Triple<Integer,Integer,Integer>>(){
 
             @Override
@@ -76,18 +73,33 @@ public class Main{
             }
         });
 
-        Triple<Integer,Integer,Integer>[] pOrdenado = (Triple<Integer,Integer,Integer>[]) new Triple[P.length];
-        int j = 0;
+        // P ahora está ordenado de manera descendente con respecto a w
 
-        for (int i=0; i<P.length-1; i++){
-            if(P[i].getValue1() > P[i+1].getValue1()){
-                pOrdenado[j++] = P[i];
+        int inicio = 0;
+        int fin = 0;
+        int i = 1;
+
+        Comparator<Triple<Integer,Integer,Integer>> comparador_l = new Comparator<Triple<Integer,Integer,Integer>>(){
+
+            @Override
+            public int compare(Triple<Integer,Integer,Integer> arg0, Triple<Integer,Integer,Integer> arg1){
+                return arg0.getValue1() - arg1.getValue1();
             }
+        };
+
+        while (i < P.length){
+            if(P[inicio].getValue0() != P[i].getValue0()){
+                fin = i-1;
+                Arrays.sort(P, inicio, fin);
+                inicio = i;
+            }
+            i++;
         }
 
-        pOrdenado[j++] = P[P.length-1];
+        // P ahora está ordenado con respecto a w y los que tienen igual w 
+        // están ordenados de manera ascendente con respecto a l
 
-        return new SimpleEntry<Triple<Integer,Integer,Integer>[], Integer>(pOrdenado, j);
+        return P;
     }
 }
 
