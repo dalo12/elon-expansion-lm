60c82d4acd15d02ef19a224b010029a482dbf669
David López <dalo_vidpez@hotmail.com.ar>
2023-05-16 12:05:34 -0300

    Implementado con LIS. Faltan corregir errores

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ public class Main{
         int n = 0;
         int W = 0;
         int L = 0;
+        int j = 0;
+
         Triple<Integer,Integer,Integer>[] P;
         Scanner s = new Scanner(System.in);
 
@@ -24,10 +26,12 @@ public class Main{
         for(int i=0; i<n; i++){
             int newW = s.nextInt();
             int newL = s.nextInt();
-            P[i] = new Triple<Integer,Integer,Integer>(newW, newL, i+1);
+            if(newW > W && newL > L) {
+                P[j++] = new Triple<Integer, Integer, Integer>(newW, newL, i + 1);
+            }
         }
 
-        Entry<Integer, List<Integer>> solucion = expandir(P, W, L);
+        Entry<Integer, List<Integer>> solucion = expandir(P, j);
 
         System.out.println(solucion.getKey());
         for(Integer i : solucion.getValue()){
@@ -35,33 +39,16 @@ public class Main{
         }
     }
 
-    protected static Entry<Integer, List<Integer>> expandir(Triple<Integer,Integer,Integer>[] P, int W, int L){
-        Triple<Integer,Integer,Integer>[] pOrdenado = ordenar(P);
-        int maxIndice = pOrdenado.length - 1;
-        int[] R = new int[pOrdenado.length];
-        List<Integer> indicesPlanes = new ArrayList<Integer>();
+    protected static Entry<Integer, List<Integer>> expandir(Triple<Integer,Integer,Integer>[] P, int cantElementos){
 
-        if( W < pOrdenado[maxIndice].getValue0() && L < pOrdenado[maxIndice].getValue1() ){
-            R[maxIndice] = 1;
-            W = pOrdenado[maxIndice].getValue0();
-            L = pOrdenado[maxIndice].getValue1();
-            indicesPlanes.add(pOrdenado[maxIndice].getValue2());
+        if (cantElementos > 0){
+            Triple<Integer,Integer,Integer>[] pOrdenado = ordenar(P);
+            return lis(pOrdenado, pOrdenado.length);
         }else{
-            R[maxIndice] = 0;
+            return new SimpleEntry<Integer, List<Integer>>(0, new ArrayList<Integer>());
         }
 
-        for(int i=maxIndice-1; i>=0; i--){
-            if(W < pOrdenado[i].getValue0() && L < pOrdenado[i].getValue1()){
-                R[i] = 1 + R[i+1];
-                W = pOrdenado[i].getValue0();
-                L = pOrdenado[i].getValue1();
-                indicesPlanes.add(pOrdenado[i].getValue2());
-            }else{
-                R[i] = R[i+1];
-            }
-        }
 
-        return new SimpleEntry(R[0], indicesPlanes);
     }
 
     protected static Triple<Integer,Integer,Integer>[] ordenar(Triple<Integer,Integer,Integer>[] P){
@@ -69,14 +56,13 @@ public class Main{
 
             @Override
             public int compare(Triple<Integer,Integer,Integer> arg0, Triple<Integer,Integer,Integer> arg1){
-                return arg1.getValue0() - arg0.getValue0();
+                return arg0.getValue0() - arg1.getValue0();
             }
         });
 
-        // P ahora está ordenado de manera descendente con respecto a w
+        // P ahora está ordenado de manera ascendente con respecto a w
 
         int inicio = 0;
-        int fin = 0;
         int i = 1;
 
         Comparator<Triple<Integer,Integer,Integer>> comparador_l = new Comparator<Triple<Integer,Integer,Integer>>(){
@@ -89,24 +75,66 @@ public class Main{
 
         while (i < P.length){
             if(P[inicio].getValue0() != P[i].getValue0()){
-                fin = i-1;
-                Arrays.sort(P, inicio, fin, comparador_l);
+                Arrays.sort(P, inicio, i, comparador_l);
                 inicio = i;
             }
             i++;
         }
 
         if(P[inicio].getValue0() == P[P.length-1].getValue0()){
-            Arrays.sort(P, inicio, P.length-1, comparador_l);
+            Arrays.sort(P, inicio, P.length, comparador_l);
         }
 
-        // P ahora está ordenado de manera descendente con respecto a w y 
+        // P ahora está ordenado de manera ascendente con respecto a w y
         // los que tienen igual w están ordenados de manera descendente con respecto a l
 
         return P;
     }
-}
 
+    /**
+     * This code is contributed by Rajat Mishra. Adaptado para la solución
+     * particular por la comisión López-Migliaro
+     * @param arr
+     * @param n
+     * @return
+     */
+
+    //protected static Entry<Integer, List<Integer>> lis(Triple<Integer,Integer,Integer>[] P, int W, int L)
+    static Entry<Integer, List<Integer>> lis(Triple<Integer,Integer,Integer>[] arr, int n) {
+        int lis[] = new int[n];
+        List<Integer>[] soluciones = (ArrayList<Integer>[]) new ArrayList[n];
+
+        // Initialize LIS values for all indexes
+        for (int i = 0; i < n; i++) {
+            soluciones[i] = new ArrayList<Integer>();
+            soluciones[i].add(arr[i].getValue2());
+            lis[i] = 1;
+        }
+
+        // Compute optimized LIS values in
+        // bottom up manner
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < i; j++) {
+                if (arr[i].getValue1() > arr[j].getValue1() && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                    soluciones[i].add(arr[j].getValue2());
+                }
+            }
+        }
+
+        // Pick maximum of all LIS values
+        int max = 0;
+        int indiceSolucion = 0;
+        for (int i = 0; i < n; i++) {
+            if (max < lis[i]) {
+                max = lis[i];
+                indiceSolucion = i;
+            }
+        }
+
+        return new SimpleEntry<Integer, List<Integer>>(max, soluciones[indiceSolucion]);
+    }
+}
 class Triple<L,M,R>{
     protected L left;
     protected M middle;
